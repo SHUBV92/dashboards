2d40c78777126abc29e513f795824ad50b20cffc
b <b>
2021-11-16 15:52:39 +0000

    Added: Set Up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PositionsPanel from './components/PositionsPanel/PositionsPanel';
+import AccordionContainer from './containers/AccordionContainer/AccordionContainer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='info-panel'>
+        <AccordionContainer />
+      </div>
+      <div className='tools-container'>
+        <div className='tools-2-container'>
+          <div className='allocation-info-panel'>Allocation Info Panel</div>
+          <div className='performance-panel'>Performance Panel</div>
+        </div>
+        <div className='positions'>
+          Positions Panel
+          <PositionsPanel />
+        </div>
+      </div>
     </div>
   );
 }
