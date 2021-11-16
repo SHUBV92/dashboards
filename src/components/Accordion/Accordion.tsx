4f57f014abca5aae7eb1429557e322d997572d7a
b <b>
2021-11-16 15:53:28 +0000

    Added: Accordion component and container

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import './Accordion.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
+
+interface AccordionProps {
+  title: string;
+  content: string;
+}
+
+const Accordion = ({ title, content }: AccordionProps) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className='accordion-container'>
+      <div onClick={handleOpen} className='accordion-title'>
+        {title}
+        <span className='accordion-icon'>
+          <FontAwesomeIcon icon={open ? faCaretUp : faCaretDown} />
+        </span>
+      </div>
+      {open && <div className='accordion-content'>{content}</div>}
+    </div>
+  );
+};
+export default Accordion;
---
+++ src/containers/AccordionContainer/AccordionContainer.tsx
@@ -0,0 +1,11 @@
+import Accordion from '../../components/Accordion/Accordion';
+
+const AccordionContainer = () => {
+  return (
+    <div>
+      <Accordion title={'London'} content={'It is a bigsss deal'} />
+    </div>
+  );
+};
+
+export default AccordionContainer;
