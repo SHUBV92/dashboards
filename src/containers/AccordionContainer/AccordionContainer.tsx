2968a403b1215f65ccc725ba73454374c7497447
b <b>
2021-11-16 17:25:26 +0000

    Updates

--- src/containers/AccordionContainer/AccordionContainer.tsx
+++ src/containers/AccordionContainer/AccordionContainer.tsx
@@ -1,9 +1,13 @@
 import Accordion from '../../components/Accordion/Accordion';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../../store/rootReducer';
 
 const AccordionContainer = () => {
+  const data = useSelector((state: RootState) => state.data.data);
+
   return (
     <div>
-      <Accordion title={'London'} content={'It is a bigsss deal'} />
+      <Accordion title={data} content={'It is a bigsss deal'} />
     </div>
   );
 };
