2de432c445f7749071573c7e04a3898d8d4ee683
b <b>
2021-11-16 23:57:57 +0000

    Update: Navbar

--- src/containers/NavBar/NavBar.styles.ts
+++ src/containers/NavBar/NavBar.styles.ts
@@ -3,12 +3,19 @@ import styled from 'styled-components'
 export const NavContainer = styled.div`
     display: flex;
     flex-direction:row;
-    width: 100vw;
     position: fixed;
     top:0;
     left: 0;
+    right: 0;
     background-color: blue;
     padding: 1rem;
     justify-content: space-between;
     color: white;
+`
+
+export const NavLinks = styled.div`
+    .a {
+        color: white;
+    }
+
 `
--- src/containers/NavBar/NavBar.tsx
+++ src/containers/NavBar/NavBar.tsx
@@ -1,13 +1,24 @@
-import { NavContainer } from './NavBar.styles';
+import { NavContainer, NavLinks } from './NavBar.styles';
+import { Link } from 'react-router-dom';
+import { RootState } from '../../store/rootReducer';
+import { useSelector } from 'react-redux';
 
 const NavBar = () => {
+  const currentUser = useSelector((state: RootState) => state.currentUser.name);
+
+  console.log('Current User from navbar', currentUser);
+
   return (
     <NavContainer>
       <div>
         <h3>Dashboards</h3>
       </div>
-      <div>Routes</div>
-      <div>User Avatar</div>
+      <NavLinks>
+        <Link to='/'>Home</Link>
+        <Link to='/dashboard'>Dashboard</Link>
+        <Link to='/login'>Sign out</Link>
+      </NavLinks>
+      <div>Signed In as: {currentUser} </div>
     </NavContainer>
   );
 };
