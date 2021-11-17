b615ec7c8a8a72b4169201f253f75abebd3c2197
b <b>
2021-11-17 14:49:38 +0000

    Update: Layout of the application

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,14 @@ import './App.css';
 import PrivateRoutesContainer from './containers/PrivateRoutesContainer/PrivateRoutesContainer';
 import NavBar from './containers/NavBar/NavBar';
 import useData from './hooks/useData';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useParams,
-  Navigate,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './containers/Dashboard/Dashboard';
 import Home from './containers/Home/Home';
 import Login from './containers/Login/Login';
+import Footer from './containers/Footer/Footer';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [loading, data] = useData(
     'https://jsonplaceholder.typicode.com/todos/1'
   );
@@ -28,12 +23,17 @@ function App() {
     <Router>
       <div className='App'>
         <NavBar />
-        {isLoggedIn && <PrivateRoutesContainer isLoggedIn={isLoggedIn} />}
-        <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/' element={<Home />} />
-        </Routes>
+        <div className='body'>
+          {isLoggedIn && <PrivateRoutesContainer isLoggedIn={isLoggedIn} />}
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </div>
+        <div className='footer'>
+          <Footer />
+        </div>
       </div>
     </Router>
   );
