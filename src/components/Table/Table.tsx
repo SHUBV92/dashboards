324375024aae7f419ab2314f9c74615e67684a66
b <b>
2021-11-23 14:05:51 +0000

    Added: Table

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,5 +1,68 @@
-const Table = () => {
-  return <div></div>;
+import { useTable, useFilters } from 'react-table';
+import React, { useState, SyntheticEvent } from 'react';
+interface TableProps {
+  columns: any;
+  data: Array<Object>;
+}
+const Table = ({ columns, data }: TableProps) => {
+  const [filterInput, setFilterInput] = useState('');
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    setFilter,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useFilters
+  );
+
+  const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setFilter('show.name', value);
+    setFilterInput(value);
+  };
+
+  return (
+    <>
+      <input
+        value={filterInput}
+        onChange={handleFilterChange}
+        placeholder={'Search name'}
+      />
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row); // this line is necessary to prepare the rows and get the row props from react-table dynamically
+
+            // Each row can be rendered directly as a string using the react table render method
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default Table;
