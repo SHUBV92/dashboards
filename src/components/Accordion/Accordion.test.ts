9f814990aab704d9e21fe8da879f5f973c4d2bc9
b <b>
2021-11-23 14:01:16 +0000

    Updated: Accordion

---
+++ src/components/Accordion/Accordion.test.ts
@@ -0,0 +1,25 @@
+import { screen, render } from '@testing-library/react'
+import  Accordion  from './Accordion'
+
+
+describe("Accordion", () => {
+    it("Accordion", () => {
+
+        const props = {
+            id:1,
+            title:"Crypto Currency ",
+            content:"Shubs",
+            avatar:true,
+            handleOpen:jest.fn(),
+            open:true,
+        }
+        const { getByText } = render(<Accordion 
+            id=1
+            title="Crypto Currency "
+            content="Shubs",
+            avatar=true
+            handleOpen=jest.fn()
+            open=true
+            />)
+    })
+})
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,22 +1,30 @@
-import { useState } from 'react';
 import './Accordion.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
+import Avatar, { genConfig } from 'react-nice-avatar';
 
 interface AccordionProps {
+  id: number;
   title: string;
   content: string | string[];
+  avatar: boolean;
+  handleOpen: (id: any) => void;
+  open: boolean;
 }
 
-const Accordion = ({ title, content }: AccordionProps) => {
-  const [open, setOpen] = useState<boolean>(false);
-  const handleOpen = () => {
-    setOpen(!open);
-  };
+const Accordion = ({
+  id,
+  title,
+  content,
+  avatar,
+  handleOpen,
+  open,
+}: AccordionProps) => {
+  const handleClick = (path: any) => console.log(`${path} clicked`);
 
   return (
     <div className='accordion-container'>
-      <div onClick={handleOpen} className='accordion-title'>
+      <div onClick={() => handleOpen(id)} className='accordion-title'>
         {title}
         <span className='accordion-icon'>
           <FontAwesomeIcon icon={open ? faCaretUp : faCaretDown} />
@@ -26,7 +34,8 @@ const Accordion = ({ title, content }: AccordionProps) => {
         <div className='accordion-content'>
           {typeof content === 'object' ? (
             content.map((item) => (
-              <ul>
+              <ul onClick={() => handleClick(item)}>
+                {avatar && <Avatar style={{ width: '3rem', height: '3rem' }} />}
                 <li>{item}</li>
               </ul>
             ))
